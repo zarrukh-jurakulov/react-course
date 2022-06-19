26153775ddaf34879486b89321f85091a913388c
Jasur <zarrukhjurakulov474@gmail.com>
2022-06-19 12:02:16 +0500

    added dekete function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,16 @@
 function Todo(props) {
+  const deleteHandler = () => {
+    console.log("Clicked");
+    console.log(props.text);
+  };
   return (
-    <div>
-      <div className='card'>
-        {props.text}
-        <h2>TITLE</h2>
-        <div className='actions'>
-          <button className='btn'>Delete</button>
-        </div>
+    <div className='card'>
+      <h2>{props.text}</h2>
+      <h2>TITLE</h2>
+      <div className='actions'>
+        <button className='btn' onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
