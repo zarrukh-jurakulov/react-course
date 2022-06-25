04b30c307924f168c4bc0d0464f8b52475665043
Zarrukh Jurakulov <zarrukhjurakulov474@gmail.com>
2022-06-25 12:04:58 +0500

    form, firebase,

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,10 +1,27 @@
+import { useHistory } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
+  const history = useHistory();
+
+  function addMeetUpHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-bcd8a-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetUpHandler} />
     </section>
   );
 }
