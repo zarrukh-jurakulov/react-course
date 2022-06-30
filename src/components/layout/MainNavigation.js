165a5809dab42a918d310f1d684576dc69c6469e
Zarrukh Jurakulov <zarrukhjurakulov474@gmail.com>
2022-06-30 15:42:22 +0500

    summary

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,10 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
+import FavoritesContext from "../../store/favorites-context";
 
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
   return (
     <header className={classes.header}>
       <div>React Meetups</div>
@@ -14,7 +17,12 @@ function MainNavigation() {
             <Link to={"/new-meetup"}>Add New Meetups</Link>
           </li>
           <li>
-            <Link to={"/favorites"}>My Favorites</Link>
+            <Link to={"/favorites"}>
+              My Favorites{" "}
+              <span className={classes.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>{" "}
+            </Link>
           </li>
         </ul>
       </nav>
